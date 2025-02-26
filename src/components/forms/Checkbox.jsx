530fdd412be7672039372188744b8e3a2926e382
Demasse <demassedimas2006@gmail.com>
2025-02-26 15:34:52 +0100

    checkbox

--- src/components/forms/Checkbox.jsx
+++ src/components/forms/Checkbox.jsx
@@ -1,3 +1,10 @@
 export function Checkbox({ checkbox, onChange, label}){
-    return <div></div>
+    return <div>
+        <input type="checkbox" 
+        className=" form-check-input"
+        checked={checked}
+        onChange={(e) =>onchange(e.target.checked)}
+        />
+        <label  className=" form-check-label" >{label}</label>
+         </div>
 }
